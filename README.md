7aa8fecb4755e00f3e45ed9163954a0d80f02ab7
Hongyuan Qiu <hongyuan@MacBook-Pro.local>
2021-05-14 22:25:00 -0230

    Modify README.md and __init__.py

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Learn SQLAlchemy and Alembic
-Learn SQLAlchemy and Alembic
+1. Create and activate the Python virtual environment in the terminal or command prompt.
+2. Install the dependencies:
+
+   `pip install -r requirements.txt`
+   
+3. Go to the data folder, then:
+
+    `(venv) $ pytest`
+    
+
+
--- data/__init__.py
+++ data/__init__.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,8 +8,6 @@ from .models import Base
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 db_url = os.environ.get("DATABASE_URL") or \
          "sqlite:///" + os.path.join(basedir, "app.db")
-if 'pytest' in sys.argv[0]:
-    db_url += '_test'
 engine = create_engine(db_url, echo=False)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
