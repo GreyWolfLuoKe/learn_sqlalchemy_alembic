0b30fb5159f9e8eb8a0bf807dba5d0fa8ccf84bf
Hongyuan Qiu <hongyuan@MacBook-Pro.local>
2021-05-16 14:55:33 -0230

    comment create_all in __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 db_url = os.environ.get("DATABASE_URL") or \
          "sqlite:///" + os.path.join(basedir, "app.db")
 engine = create_engine(db_url, echo=False)
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
 from data.db import CrudTable
